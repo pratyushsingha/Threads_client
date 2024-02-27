f6b73a820cbf90e0ba61fdb2a7d4172a381b382d
pratyushsingha <pratyushsingha83@gmail.com>
2024-02-27 10:27:01 +0530

    feat: signup & login functionality added

--- src/main.jsx
+++ src/main.jsx
@@ -4,15 +4,35 @@ import App from "./App.jsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home.jsx";
+import Register from "./pages/auth/Register.jsx";
+import AppContextProvider from "./context/AppContext.jsx";
+import { Toaster } from "./components/ui/toaster.jsx";
+import { ThemeProvider } from "./context/theme-provider.jsx";
+import Login from "./pages/auth/Login.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    children: [{ path: "/", element: <Home /> }],
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <AppContextProvider>
+      <RouterProvider router={router} />
+      <Toaster />
+    </AppContextProvider>
+  </ThemeProvider>
 );
