99d9721dd93cf9c5695998b78f5c4f1b436b8098
pratyushsingha <pratyushsingha83@gmail.com>
2024-03-01 12:36:52 +0530

    feat: tweet like,bookmark,share feature added with infinty scroll

---
+++ src/components/Index.jsx
@@ -0,0 +1,56 @@
+import { sidebarItems } from "@/utils";
+import { Button } from "./ui/button";
+import { AppContext } from "@/context/AppContext";
+import { useToast } from "@/components/ui/use-toast";
+import { Toaster } from "@/components/ui/toaster.jsx";
+import { ThemeProvider } from "@/context/theme-provider.jsx";
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+import Login from "@/pages/auth/Login.jsx";
+import App from "@/App";
+import Home from "@/pages/Home";
+import AppContextProvider from "@/context/AppContext";
+import Register from "@/pages/auth/Register.jsx";
+import TweetCard from "@/components/TweetCard";
+import Spinner from "@/components/loader/Spinner";
+import Container from "@/components/Container";
+import Sidebar from "@/components/Sidebar";
+
+export {
+  sidebarItems,
+  Button,
+  Container,
+  Sidebar,
+  AppContext,
+  TweetCard,
+  Spinner,
+  useToast,
+  Home,
+  Register,
+  AppContextProvider,
+  Toaster,
+  ThemeProvider,
+  Login,
+  App,
+  Label,
+  Input,
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+};
---
+++ src/utils/index.jsx
@@ -0,0 +1,29 @@
+import { Home, BellDot, Mails, Bot, BookmarkCheck } from "lucide-react";
+
+export const sidebarItems = [
+  {
+    _id: 1,
+    title: "Home",
+    icon: <Home />,
+  },
+  {
+    _id: 2,
+    title: "Notifications",
+    icon: <BellDot />,
+  },
+  {
+    _id: 3,
+    title: "Messages",
+    icon: <Mails />,
+  },
+  {
+    _id: 4,
+    title: "Grok",
+    icon: <Bot />,
+  },
+  {
+    _id: 5,
+    title: "Bookmarks",
+    icon: <BookmarkCheck />,
+  },
+];
