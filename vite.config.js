1603c0518fb6dcdf117640c3aac25110cb175774
Pratyush Singha <pratyushsingha83@gmail.com>
2025-07-27 03:53:13 +0530

    feat: update Vite configuration for improved server and preview settings

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,25 @@ import react from "@vitejs/plugin-react-swc"
 import { defineConfig } from "vite"
 
 export default defineConfig({
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  preview: {
+    port: 4173,
+    strictPort: true,
+  },
+  server: {
+    port: 8081,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8081",
+  },
   optimizeDeps: {
-    exclude: ['js-big-decimal']
-  }
+    exclude: ["js-big-decimal"],
+  },
 })
+
